f45f50bb72927576eb2cf36be619a2632eeb9a21
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 15:33:33 +0100

    Initial Commit

---
+++ Notification/Notification.h
@@ -0,0 +1,19 @@
+//
+//  Notification.h
+//  Notification
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Notification.
+FOUNDATION_EXPORT double NotificationVersionNumber;
+
+//! Project version string for Notification.
+FOUNDATION_EXPORT const unsigned char NotificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Notification/PublicHeader.h>
+
+
